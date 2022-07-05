9215e7ffb1dad5130b4d2fa67566cb812a1ee432
joe-elmoufak <Joe.elmoufak@lifeworks.com>
2022-07-05 23:20:02 +0100

    Update CucumberTestRunnerTest.java

--- src/test/java/runners/CucumberTestRunnerTest.java
+++ src/test/java/runners/CucumberTestRunnerTest.java
@@ -1,14 +1,15 @@
 package runners;
 
-
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/features",glue = {"stepdefinitions", "utils"},
+@CucumberOptions(features = "src/test/resources/Features",glue = {"stepdefinitions", "utils"},
         monochrome = true,
-        plugin = {"pretty", "html:tardget/Cucumber-Reports"}
+        plugin = {
+        "pretty",
+        "html:target/cucumber-report/cucumber.html"}
 )
 public class CucumberTestRunnerTest {
 }
